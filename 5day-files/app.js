c001af77acd0db6212ef1b3ce7cae0726b3e77d0
PythonicVirtuoso <girozil@hotmail.com>
2023-03-28 20:30:39 +0200

    functional commit of button system

--- 5day-files/app.js
+++ 5day-files/app.js
@@ -1,87 +1,92 @@
-const dayOneWorkout = document.querySelectorAll("form");
+// Try to work from an empty slate at first, if you need it, look
+// at github for inspiration.
+
+const form = document.querySelector("form");
 const first = document.querySelector(".first");
 const second = document.querySelector(".second");
 const third = document.querySelector(".third");
 const fourth = document.querySelector(".fourth");
-const fifth = document.querySelector(".fifth");
-const sixth = document.querySelector(".sixth");
-const seventh = document.querySelector(".seventh");
-const eight = document.querySelector(".eight");
-const ninth = document.querySelector(".ninth");
-const tenth = document.querySelector(".tenth");
 
-dayOneWorkout.forEach((form) =>
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const textField = form.textField.value;
-    form.reset();
+/* Maybe create an index for the different buttons. And if the buttons
+qualify, accept it, if not, return false.
+Also check if I need to disable both the click and the submit events. */
 
-    if (e.target.classList.contains("firstForm")) {
-      updateUI(textField, 0);
-    } else if (e.target.classList.contains("secondForm")) {
-      updateUI(textField, 1);
-    } else if (e.target.classList.contains("thirdForm")) {
-      updateUI(textField, 2);
-    } else if (e.target.classList.contains("fourthForm")) {
-      updateUI(textField, 3);
-    } else if (e.target.classList.contains("fifthForm")) {
-      updateUI(textField, 4);
-    } else if (e.target.classList.contains("sixthForm")) {
-      updateUI(textField, 5);
-    } else if (e.target.classList.contains("seventhForm")) {
-      updateUI(textField, 6);
-    } else if (e.target.classList.contains("eightForm")) {
-      updateUI(textField, 7);
-    } else if (e.target.classList.contains("ninthForm")) {
-      updateUI(textField, 8);
-    } else if (e.target.classList.contains("tenthForm")) {
-      updateUI(textField, 9);
-    }
-  })
-);
-
-const updateUI = (textField, number) => {
-  console.log(textField, number);
-
-  if (number === 0) {
-    first.innerHTML += `
-      <li>${textField}</li>
-      `;
-  } else if (number === 1) {
-    second.innerHTML += `
-    <li>${textField}</li>
-    `;
-  } else if (number === 2) {
-    third.innerHTML += `
-    <li>${textField}</li>
-    `;
-  } else if (number === 3) {
-    fourth.innerHTML += `
-    <li>${textField}</li>
-    `;
-  } else if (number === 4) {
-    fifth.innerHTML += `
-    <li>${textField}</li>
-    `;
-  } else if (number === 5) {
-    sixth.innerHTML += `
-    <li>${textField}</li>
-    `;
-  } else if (number === 6) {
-    seventh.innerHTML += `
-    <li>${textField}</li>
-    `;
-  } else if (number === 7) {
-    eight.innerHTML += `
-    <li>${textField}</li>
-    `;
-  } else if (number === 8) {
-    ninth.innerHTML += `
-    <li>${textField}</li>
-    `;
-  } else if (number === 9) {
-    tenth.innerHTML += `
-    <li>${textField}</li>
-    `;
-  }
+func = () => {
+  form.addEventListener(
+    "submit",
+    (e) => {
+      e.preventDefault();
+      console.log(e);
+      const textField = form.textField.value;
+      form.reset();
+      first.innerHTML += `
+        <li>${textField}</li>
+        `;
+      {
+      }
+    },
+    { once: true }
+  );
 };
+
+func1 = () => {
+  form.addEventListener(
+    "submit",
+    (e) => {
+      e.preventDefault();
+      console.log(e);
+      const textField = form.textField.value;
+      form.reset();
+      second.innerHTML += `
+        <li>${textField}</li>
+        `;
+    },
+    { once: true }
+  );
+};
+
+func2 = () => {
+  form.addEventListener(
+    "submit",
+    (e) => {
+      e.preventDefault();
+      console.log(e);
+      const textField = form.textField.value;
+      form.reset();
+      third.innerHTML += `
+        <li>${textField}</li>
+        `;
+    },
+    { once: true }
+  );
+};
+
+func3 = () => {
+  form.addEventListener(
+    "submit",
+    (e) => {
+      e.preventDefault();
+      console.log(e);
+      const textField = form.textField.value;
+      form.reset();
+      fourth.innerHTML += `
+        <li>${textField}</li>
+        `;
+    },
+    { once: true }
+  );
+};
+/*
+buttons.forEach((button)) =>
+  button.addEventListener("click", (e) => {
+    whichButton = e.target.classList
+    if (whichButton.contains("firstButton")){
+        first.innerHTML += `
+        <li>${textField}</li>
+        `;
+    } else if...
+
+  },
+  {once: true}
+)
+*/
