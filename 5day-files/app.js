2cdaa3222941a0bc85ebea2af0d096c3c134dd78
PythonicVirtuoso <girozil@hotmail.com>
2023-03-29 21:20:47 +0200

    working version

--- 5day-files/app.js
+++ 5day-files/app.js
@@ -1,92 +1,75 @@
 // Try to work from an empty slate at first, if you need it, look
 // at github for inspiration.
 
+const buttons = document.querySelectorAll("button");
 const form = document.querySelector("form");
 const first = document.querySelector(".first");
 const second = document.querySelector(".second");
 const third = document.querySelector(".third");
 const fourth = document.querySelector(".fourth");
+const fifth = document.querySelector(".fifth");
+const sixth = document.querySelector(".sixth");
+const seventh = document.querySelector(".seventh");
+const eight = document.querySelector(".eight");
+const ninth = document.querySelector(".ninth");
+const tenth = document.querySelector(".tenth");
 
-/* Maybe create an index for the different buttons. And if the buttons
-qualify, accept it, if not, return false.
-Also check if I need to disable both the click and the submit events. */
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    whichButton = e.target.classList;
+    addForm(whichButton);
+  })
+);
 
-func = () => {
+const addForm = (whichButton) => {
   form.addEventListener(
     "submit",
     (e) => {
       e.preventDefault();
-      console.log(e);
-      const textField = form.textField.value;
+      textField = form.textField.value;
       form.reset();
-      first.innerHTML += `
-        <li>${textField}</li>
-        `;
-      {
+      if (whichButton.contains("firstButton")) {
+        first.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("secondButton")) {
+        second.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("thirdButton")) {
+        third.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("fourthButton")) {
+        fourth.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("fifthButton")) {
+        fifth.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("sixthButton")) {
+        sixth.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("seventhButton")) {
+        seventh.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("eightButton")) {
+        eigth.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("ninthButton")) {
+        ninth.innerHTML += `
+      <li>${textField}</li>
+      `;
+      } else if (whichButton.contains("tenthButton")) {
+        tenth.innerHTML += `
+      <li>${textField}</li>
+      `;
       }
     },
     { once: true }
   );
 };
-
-func1 = () => {
-  form.addEventListener(
-    "submit",
-    (e) => {
-      e.preventDefault();
-      console.log(e);
-      const textField = form.textField.value;
-      form.reset();
-      second.innerHTML += `
-        <li>${textField}</li>
-        `;
-    },
-    { once: true }
-  );
-};
-
-func2 = () => {
-  form.addEventListener(
-    "submit",
-    (e) => {
-      e.preventDefault();
-      console.log(e);
-      const textField = form.textField.value;
-      form.reset();
-      third.innerHTML += `
-        <li>${textField}</li>
-        `;
-    },
-    { once: true }
-  );
-};
-
-func3 = () => {
-  form.addEventListener(
-    "submit",
-    (e) => {
-      e.preventDefault();
-      console.log(e);
-      const textField = form.textField.value;
-      form.reset();
-      fourth.innerHTML += `
-        <li>${textField}</li>
-        `;
-    },
-    { once: true }
-  );
-};
-/*
-buttons.forEach((button)) =>
-  button.addEventListener("click", (e) => {
-    whichButton = e.target.classList
-    if (whichButton.contains("firstButton")){
-        first.innerHTML += `
-        <li>${textField}</li>
-        `;
-    } else if...
-
-  },
-  {once: true}
-)
-*/
