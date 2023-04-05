3206f503bc3c894e91dc663af21c546f018a23d8
PythonicVirtuoso <girozil@hotmail.com>
2023-04-05 20:12:58 +0200

    ..

--- 5day-files/app.js
+++ 5day-files/app.js
@@ -12,73 +12,69 @@ const eight = document.querySelector(".eight");
 const ninth = document.querySelector(".ninth");
 const tenth = document.querySelector(".tenth");
 
-// add an event listener for every click I make in the browser
-buttons.forEach((button) =>
-  button.addEventListener("click", (e) => {
-    whichButton = e.target.classList;
-    addForm(whichButton);
-    toggleButtons(e);
-  })
-);
-
-// change the color of the buttons, according to which one you last
-// pressed on
 const toggleButtons = (e) => {
   buttons.forEach((button) => button.classList.remove("btn"));
   e.target.classList.add("btn");
 };
 
-// add eventlistener to the form which puts the text you write into
-// it into the UL field under the button you pressed.
-const addForm = (whichButton) => {
-  form.addEventListener(
-    "submit",
-    (e) => {
-      e.preventDefault();
-      textField = form.textField.value;
-      form.reset();
-      if (whichButton.contains("firstButton") && textField.length) {
-        first.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("secondButton")) {
-        second.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("thirdButton")) {
-        third.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("fourthButton")) {
-        fourth.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("fifthButton")) {
-        fifth.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("sixthButton")) {
-        sixth.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("seventhButton")) {
-        seventh.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("eightButton")) {
-        eigth.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("ninthButton")) {
-        ninth.innerHTML += `
-      <li>${textField}</li>
-      `;
-      } else if (whichButton.contains("tenthButton")) {
-        tenth.innerHTML += `
-      <li>${textField}</li>
-      `;
-      }
-    },
-    { once: true }
-  );
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const textField = form.textField.value;
+  e.target.reset();
+
+  const btn = document.querySelector("button.btn");
+  if (!btn) {
+    return;
+  }
+  const classList = btn.classList;
+
+  if (classList.contains("firstButton") && textField.length) {
+    first.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("secondButton")) {
+    second.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("thirdButton")) {
+    third.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("fourthButton")) {
+    fourth.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("fifthButton")) {
+    fifth.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("sixthButton")) {
+    sixth.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("seventhButton")) {
+    seventh.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("eightButton")) {
+    eight.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("ninthButton")) {
+    ninth.innerHTML += `
+  <li>${textField}</li>
+  `;
+  } else if (classList.contains("tenthButton")) {
+    tenth.innerHTML += `
+  <li>${textField}</li>
+  `;
+  }
 };
+
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    toggleButtons(e);
+    window.removeEventListener(form, onFormSubmit);
+    form.addEventListener("submit", onFormSubmit);
+  })
+);
